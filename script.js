9823afb360cfb440e55ae1ececb3f1eba44e858d
Robbie-Netizen <robbie@tringali.me>
2026-04-24 13:57:07 -0700

    Added eventListeners for pop-ups

--- script.js
+++ script.js
@@ -1,28 +1,77 @@
 const eecuCard = document.getElementById('card-eecu');
-const eecuCardExpanded = document.getElementById('expanded-card-eecu')
+const eecuCardExpanded = document.getElementById('expanded-card-eecu');
+
 const toDoCard = document.getElementById('card-to-do');
-const todoCardExpanded = document.getElementById('expanded-card-to-do')
+const toDoCardExpanded = document.getElementById('expanded-card-to-do');
+
 const looplessCard = document.getElementById('card-loopless');
-const looplessCardExpanded = document.getElementById('expanded-card-loopless')
+const looplessCardExpanded = document.getElementById('expanded-card-loopless');
+
 const animalCard = document.getElementById('card-animal');
-const animalCardExpanded = document.getElementById('expanded-card-animal')
+const animalCardExpanded = document.getElementById('expanded-card-animal');
+
 const blackHoldCard = document.getElementById('card-black-hole');
-const expandedCardView = document.getElementById('large-card-holder')
+const blackHoldCardExpanded = document.getElementById('expanded-card-black-hole')
+
+const expandedCardView = document.getElementById('large-card-holder');
 
 
-eecuCard.addEventListener('click', (eecu) => {
+eecuCard.addEventListener('click', () => {
     expandCard("eecu");
-});
+}, true);
 
-eecuCardExpanded.addEventListener('click', (eecu) => {
-    expandCard("eecu");
-});
+
+eecuCardExpanded.addEventListener('click', () => {
+    minimizeCard("eecu");
+}, true);
+
+toDoCard.addEventListener('click', () => {
+    expandCard("to-do");
+}, true);
+
+
+toDoCardExpanded.addEventListener('click', () => {
+    minimizeCard("to-do");
+}, true);
+
+
+looplessCard.addEventListener('click', () => {
+    expandCard("loopless");
+}, true);
+
+
+looplessCardExpanded.addEventListener('click', () => {
+    minimizeCard("loopless");
+}, true);
+
+
+animalCard.addEventListener('click', () => {
+    expandCard("animal");
+}, true);
+
+
+animalCardExpanded.addEventListener('click', () => {
+    minimizeCard("animal");
+}, true);
+
+
+blackHoldCard.addEventListener('click', () => {
+    expandCard("black-hole");
+}, true);
+
+
+blackHoldCardExpanded.addEventListener('click', () => {
+    minimizeCard("black-hole");
+}, true);
+
+
+
 
 function expandCard(project) {
     let expandedCard = document.getElementById('expanded-card-' + project);
     console.log(expandedCard);
-    expandedCard.classList.toggle('hidden');
-    expandedCard.classList.toggle('shown');
+    expandedCard.classList.remove('hidden');
+    expandedCard.classList.add('shown');
     expandedCardView.classList.remove('inactive');
     console.log(expandedCard.classList);
     console.log(expandedCardView.classList);
@@ -31,9 +80,10 @@ function expandCard(project) {
 function minimizeCard(project) {
     let expandedCard = document.getElementById('expanded-card-' + project);
     console.log(expandedCard);
-    expandedCard.classList.toggle('hidden');
-    expandedCard.classList.toggle('shown');
-    expandedCardView.classList.remove('inactive');
+    expandedCard.classList.add('hidden');
+    expandedCard.classList.remove('shown');
+    expandedCardView.classList.add('inactive');
     console.log(expandedCard.classList);
     console.log(expandedCardView.classList);
 }
+
